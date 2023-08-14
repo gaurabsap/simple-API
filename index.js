a1a4312cde0de8c4296581c911c26a483a1aac44
Gaurab sapkota <iamgaurabsap20@gmail.com>
2023-08-14 12:18:17 +0545

    fix bugs

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
+ConnectDb();
 app.listen(process.env.PORT, () => {
-  ConnectDb();
   console.log("server is running...");
 });
