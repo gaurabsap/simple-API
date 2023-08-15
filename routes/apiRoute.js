3e36d130f0bb86db88c4c18727d6d20c4f3c8684
gb_gaurab19 <86666317+gaurabsap@users.noreply.github.com>
2023-08-15 13:27:02 +0545

    Update apiRoute.js

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -10,8 +10,8 @@ routes.get("/", async (resq, resp) => {
     "Welcome to api 😃 <br><br> Routes : <br> createApi: /api/v1/create/{username} <br> getApidata: /api/data?user={username}"
   );
 });
-routes.use("*", async (resq, resp) => {
-  resp.status(404).json({
-    message: "Routes not found",
-  });
-});
+// routes.use("*", async (resq, resp) => {
+//   resp.status(404).json({
+//     message: "Routes not found",
+//   });
+// });
