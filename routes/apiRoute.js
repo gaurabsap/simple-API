d621a01f9dcae4e356ca04aace0ade2071bbc8cc
gb_gaurab19 <86666317+gaurabsap@users.noreply.github.com>
2023-08-15 13:41:12 +0545

    fix route path

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -7,7 +7,7 @@ routes.post("/api/create/:user", createJson);
 routes.get("/api/data", getJsonData);
 routes.get("/", async (resq, resp) => {
   return resp.send(
-    "Welcome to api 😃 <br><br> Routes : <br> createApi: /api/v1/create/{username} <br> getApidata: /api/data?user={username}"
+    "Welcome to api 😃 <br><br> Routes : <br> createApi: /api/create/{username} <br> getApidata: /api/data?user={username}"
   );
 });
 // routes.use("*", async (resq, resp) => {
